60529e4cb22cd9c129e37dd515ad6bd15f38ebf2
erika lopes <caluerika@gmail.com>
2022-04-25 11:20:43 -0300

    add react storybook web

---
+++ src/Button/Button.stories.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Button from "./Button";
+import { Alert } from "react-native";
+
+export default {
+  title: "Example/Hero",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const WithButton = Template.bind({});
+
+WithButton.storyName = "With button";
+WithButton.args = {
+  title: "Welcome to my App",
+  buttonText: "Sign in",
+  hideButton: false,
+  onButtonPress: () => Alert.alert("Signed in"),
+};
+
+export const WithoutButton = Template.bind({});
+
+WithoutButton.storyName = "Without button";
+WithoutButton.args = {
+  title: "Welcome to my App",
+  buttonText: "Sign in",
+  hideButton: true,
+  onButtonPress: () => Alert.alert("Signed in"),
+};
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,5 +1,29 @@
-import { TouchableOpacityBase } from "react-native";
+import React from "react";
+import { Button as ButtonRN, StyleSheet, Text, View } from "react-native";
 
-export function Button() {
-  return <TouchableOpacityBase>Click me</TouchableOpacityBase>;
-}
+type ButtonProps = {
+  title: string;
+  buttonText: string;
+  onButtonPress: () => void;
+};
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const { title, buttonText, onButtonPress } = props;
+
+  return (
+    <View>
+      <Text style={styles.title}></Text>
+      <ButtonRN title={buttonText} onPress={onButtonPress}></ButtonRN>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 32,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+});
+
+export default Button;
